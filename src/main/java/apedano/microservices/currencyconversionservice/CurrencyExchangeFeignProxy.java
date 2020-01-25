3a13770eda1559de7520853c84fdbc3de67fda1f
apedano <pedano.alessandro@gmail.com>
2020-01-25 19:09:03 +0100

    Added Ribbon basic

--- src/main/java/apedano/microservices/currencyconversionservice/CurrencyExchangeFeignProxy.java
+++ src/main/java/apedano/microservices/currencyconversionservice/CurrencyExchangeFeignProxy.java
@@ -5,6 +5,7 @@
  */
 package apedano.microservices.currencyconversionservice;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,7 +15,9 @@ import org.springframework.web.bind.annotation.PathVariable;
  * 
  * @author Alessandro
  */
-@FeignClient(name="currency-exchange-service", url="localhost:8000")
+//@FeignClient(name="currency-exchange-service", url="localhost:8000")
+@FeignClient(name="currency-exchange-service")
+@RibbonClient(name="currency-exchange-service")
 public interface CurrencyExchangeFeignProxy {
     
     /*
